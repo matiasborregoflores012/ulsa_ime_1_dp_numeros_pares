59bdc2d1d3c37898089abc86754a634392c953f8
matiasborregoflores012 <matias.borrego.flores012@gmail.com>
2026-09-24 19:39:34 -0600

    termino de hacer el if le pongo cout y cin y lo de los pares ahora sigue hacer la salida ya quedo la entrada

--- main.cpp
+++ main.cpp
@@ -12,21 +12,21 @@ int main() {
 
    int pares[CANTIDAD]; 
    int totalPares = 0;
+   int numero;
+
 
 
    
- 
-    //    TODO: declara el arreglo pares. ¿De qué tamaño en el peor caso?
-
-
-    //    TODO: declara totalPares. ¿Con qué valor empieza?
-
-
-
-
-    std::cout << "Guardar los numeros pares de " << CANTIDAD << " numeros\n";
 
     // 3. Ciclo: leer CANTIDAD números
+
+
+    for(int i = 0; i < CANTIDAD; i++){if(numero % 2==0)
+        
+        i = 0; i < totalPares;
+
+
+
     //    TODO: lee cada número con leerEntero("Escribe un numero: ")
     //    TODO: si el número es par, guárdalo en la siguiente posición libre
     //    ¿Qué variable te dice cuál es la siguiente posición libre?
